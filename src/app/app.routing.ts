fc38f4cd6bc855711c7ed42fb9cd1a166eddeb89
talel-elghali <talelelghali96@gmail.com>
2020-11-19 18:29:07 +0100

    register new client done

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
-
+import { SigninComponent } from './examples/signin/signin.component' 
 import { ComponentsComponent } from './components/components.component';
 import { ProfileComponent } from './examples/profile/profile.component';
 import { SignupComponent } from './examples/signup/signup.component';
@@ -15,7 +15,8 @@ const routes: Routes =[
     { path: 'user-profile',     component: ProfileComponent },
     { path: 'signup',           component: SignupComponent },
     { path: 'landing',          component: LandingComponent },
-    { path: 'nucleoicons',      component: NucleoiconsComponent }
+    { path: 'nucleoicons',      component: NucleoiconsComponent },
+    { path: 'signin',           component: SigninComponent },
 ];
 
 @NgModule({
