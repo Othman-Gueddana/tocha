48cebbbd33a51374c33aba820b0950132f75fe83
othman <othmangueddana@gmail.com>
2020-11-21 14:04:50 +0100

    css

--- src/app/examples/landing/landing.component.ts
+++ src/app/examples/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 @Component({
     selector: 'app-landing',
     templateUrl: './landing.component.html',
@@ -9,13 +10,31 @@ import { ProductService } from '../services/product.service';
 export class LandingComponent implements OnInit {
   focus: any;
   focus1: any;
-  products:[]
-  constructor(private server:ProductService) { }
+  products:[];
+  closeResult = '';
+  constructor(private server: ProductService, private modalService: NgbModal) { }
 
   ngOnInit() {
       this.server.getProducts().subscribe(( data: any) => {
           this.products=data
         console.log(this.products)
       })
+}
+  open(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
