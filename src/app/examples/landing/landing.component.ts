c2eb6ffcf882545d66562acc45d0e9352a099dc6
chaimathabet <benthabet.chayma@gmail.com>
2020-11-22 22:06:15 +0100

    modal is working

--- src/app/examples/landing/landing.component.ts
+++ src/app/examples/landing/landing.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input } from '@angular/core';
 import { ProductService } from '../services/product.service';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+//import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdModalComponent } from '../modal/modal.component';
+import { NgbdModalContent } from '../modal/modal.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
     selector: 'app-landing',
     templateUrl: './landing.component.html',
@@ -11,30 +14,44 @@ export class LandingComponent implements OnInit {
   focus: any;
   focus1: any;
   products:[];
-  closeResult = '';
-  constructor(private server: ProductService, private modalService: NgbModal) { }
+ 
+  constructor(private server: ProductService , private modalService: NgbModal) { }
 
   ngOnInit() {
       this.server.getProducts().subscribe(( data: any) => {
           this.products=data
         console.log(this.products)
-      })
-}
-  open(content) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
+      })}
+      open(item) {
+        debugger;
+       console.log(item)
+        const modalRef = this.modalService.open(NgbdModalContent);
+        modalRef.componentInstance.item = item;
+        modalRef.result.then((result) => {
+          if (result) {
+            console.log(result);
+          }
+        });
+       
     }
-  }
+    
 }
+
+//   open(content) {
+//     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+//       this.closeResult = `Closed with: ${result}`;
+//     }, (reason) => {
+//       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+//     });
+//   }
+
+//   private getDismissReason(reason: any): string {
+//     if (reason === ModalDismissReasons.ESC) {
+//       return 'by pressing ESC';
+//     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+//       return 'by clicking on a backdrop';
+//     } else {
+//       return `with: ${reason}`;
+//     }
+//   }
+ 
