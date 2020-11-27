34ea5671289597e03f408e7a78ccf2417dbc4628
othman <othmangueddana@gmail.com>
2020-11-27 16:05:38 +0100

    admin verify client and products

--- admin-dashboard/src/app/app.routing.ts
+++ admin-dashboard/src/app/app.routing.ts
@@ -5,18 +5,19 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
 export const AppRoutes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'products',
     pathMatch: 'full',
   }, {
     path: '',
     component: AdminLayoutComponent,
     children: [
-        {
-      path: '',
-      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
-  }]},
+      {
+        path: '',
+        loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+      }]
+  },
   {
     path: '**',
-    redirectTo: 'dashboard'
+    redirectTo: 'products'
   }
 ]
