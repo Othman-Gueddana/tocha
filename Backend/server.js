062083393cbf92758a20289b9479f6d543eb7dc3
othman <othmangueddana@gmail.com>
2020-12-08 23:40:02 +0100

    testing heroku re

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(express.static( __dirname +'../../tocha/bekhtef/dist/bekhteff'))
 
-app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "../../tocha/bekhtef/src/index.html"));
 });
 // Routes 
