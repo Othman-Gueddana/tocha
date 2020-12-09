f13132a9e8747144cc0c583cb33d6e67157249ef
othman <othmangueddana@gmail.com>
2020-12-09 22:13:53 +0100

    router in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,14 +3,14 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const path = require("path")
-
+const router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use(express.static('../bekhtef/dist/bekhteff'))
 
-app.get("/", function (req, res) {
+router.get("/", function (req, res) {
   res.sendFile(path.join(__dirname,"../bekhtef/src","index.html"));
 });
 // Routes 
